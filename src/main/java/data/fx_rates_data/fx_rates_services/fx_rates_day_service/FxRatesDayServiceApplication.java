a10e3d4fb8249bedffe2914e7495984efddd79ff
SB <amruthasuri@gmail.com>
2025-03-15 07:49:33 +0530

    Initial Commit with Spring Boot gRPC, Spring Data JPA and Postgres drivers

---
+++ src/main/java/data/fx_rates_data/fx_rates_services/fx_rates_day_service/FxRatesDayServiceApplication.java
@@ -0,0 +1,13 @@
+package data.fx_rates_data.fx_rates_services.fx_rates_day_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxRatesDayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxRatesDayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/data/fx_rates_data/fx_rates_services/fx_rates_day_service/FxRatesDayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package data.fx_rates_data.fx_rates_services.fx_rates_day_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxRatesDayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
